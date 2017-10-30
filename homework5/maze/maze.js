435a667f045f9c68b04917261fc857db80dc8b04
chenyime <15626455847@163.com>
2017-10-30 22:21:38 +0800

    homework5

---
+++ homework5/maze/maze.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    let start = false, win = true;
+    let $result = $(".result")
+
+    $(".start").mouseenter(function() {
+        start = true, win = true;
+        $(".wall").each(function() {
+            $(this).removeClass("lose");
+        })
+        $result.text("");
+        console.log(start);
+        console.log(win);
+    })
+
+    $(".wall").mouseenter(function() {
+        if (win && start) {
+            win = false;
+            $(this).addClass("lose");
+            $result.text("You Lose!");
+        }
+        
+    })
+
+    $(".end").mouseenter(function() {
+        console.log(start);
+        console.log(win);
+        if (start && win) {
+            $result.text("You Win!");
+            start = false;
+        } else if (!start && win) {
+            $result.text("Don't cheat, you should start from the 'S' and move to the 'E' inside the maze!")
+        }
+    })
+
+    $(".maze").mouseleave(function() {
+        if (start) {
+            start = false;
+        }
+    })
+})
---
+++ homework5/whac-a-mole/whac-a-mole.js
@@ -0,0 +1,93 @@
+$(document).ready(function() {
+    let time = 30, status = false, score = 0, n, hit = true, num;
+
+    function timer() {
+        return setInterval(getTime, 1000);
+    }
+
+    function getTime() {
+        time--;
+        $(".time").val(time);
+        if (time == 0) {
+            clearInterval(n);
+            status = false;
+            changeMoleStatus(false);
+            $(".status").val("Game over");
+        }
+    }
+
+    function randomNum(min, max) {
+        let range = max - min, rand = Math.random();
+        return min + Math.round(range * rand);
+    }
+
+
+    function getNum() {
+        if (hit) {
+            num = randomNum(1, 60);
+            console.log(num);
+        }
+        $(".mole").each(function() {
+            let $this = $(this);
+            if ($this.attr("key") == num.toString()) {
+                $this.focus();
+            }
+        })
+        return num;
+    }
+
+    function changeMoleStatus(state) {
+        $(".mole").each(function() {
+            let $this = $(this);
+            if (state) {
+                $this.removeAttr("disabled");
+            } else {
+                $this.attr("disabled", "true");
+            }
+        })
+    }
+
+    function freshMole() {
+        $(".mole").each(function() {
+            let $this = $(this);
+            $this.prop("checked", false);
+        })
+    }
+
+    $(".control").click(function() {
+        if (status) {
+            status = false;
+            clearInterval(n);
+            time = 0;
+            $(".time").val(time);
+            changeMoleStatus(false);
+            $(".status").val("Game over");
+        } else {
+            status = true;
+            time = 30;
+            n = timer();
+            getNum();
+            hit = false;
+            changeMoleStatus(true);
+            $(".status").val("Game start");
+        }
+    })
+
+    $(".mole").click(function() {
+        let $this = $(this);
+        freshMole();
+        if ($this.attr("key") == num.toString()) {
+            hit = true;
+            score++;
+            $this.prop("checked", false);
+        } else {
+            hit = false;
+            score--;
+            $this.prop("checked", true);
+        }
+        console.log($this.attr("key"));
+        getNum();
+        $(".score").val(score);
+    })
+
+})
